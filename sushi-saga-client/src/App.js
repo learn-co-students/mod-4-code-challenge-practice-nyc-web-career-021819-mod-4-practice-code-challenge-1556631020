587637748cde01fb0f8ab738aa53875914d67187
tchang9 <tlchang@gmail.com>
2019-04-30 11:05:41 -0400

    tried my best

--- sushi-saga-client/src/App.js
+++ sushi-saga-client/src/App.js
@@ -1,19 +1,71 @@
 import React, { Component } from 'react';
 import SushiContainer from './containers/SushiContainer';
 import Table from './containers/Table';
+import SushiWallet from './containers/SushiWallet'
 
 // Endpoint!
 const API = "http://localhost:3000/sushis"
 
 class App extends Component {
 
+  state = {
+    sushi: [],
+    budget: 100,
+    plates: []
+
+  }
+
+  getSushi = () => {
+    fetch(API)
+    .then(res => res.json())
+    .then(res=>{
+      this.setState({
+        sushi: res
+      })
+    })
+  }
+
+  addMoney = (money) => {
+    const intMonet = parseInt(money)
+    this.setState(prevState => {
+      return {
+        budget: prevState.budget + intMonet
+      }
+    })
+  }
+
+  eatenSushi = (eatenSushi) => {
+    return [...this.state.sushi].map(sushi => {
+      if (sushi.id === eatenSushi.id) {
+        return {...sushi, ...{eaten: true}}
+      } else {
+        return sushi
+      }
+    })
+  }
+
+  ateSushi = (eatenSushi) => {
+    this.setState(prevState => {
+      return {
+        sushi: this.eatenSushi(eatenSushi),
+        budget: prevState.budget - eatenSushi.price,
+        plates: [...prevState.plates, eatenSushi.price]
+      }
+    })
+  }
+
   render() {
     return (
       <div className="app">
-        <SushiContainer  />
-        <Table />
+        <SushiWallet addMoney={this.addMoney}/>
+        <SushiContainer  budget={this.state.budget} ateSushi={this.ateSushi} sushi={this.state.sushi}/>
+        <Table budget={this.state.budget} plates={this.state.plates}/>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.getSushi()
   }
 }
 
--- sushi-saga-client/src/containers/SushiContainer.js
+++ sushi-saga-client/src/containers/SushiContainer.js
@@ -1,19 +1,50 @@
 import React, { Fragment } from 'react'
 import MoreButton from '../components/MoreButton'
+import Sushi from '../components/Sushi'
+import v4 from 'uuid'
 
-const SushiContainer = (props) => {
-  return (
-    <Fragment>
-      <div className="belt">
-        {
-          /* 
-             Render Sushi components here!
-          */
-        }
-        <MoreButton />
-      </div>
-    </Fragment>
-  )
+export default class SushiContainer extends React.Component{
+
+  state = {
+    index: 0
+  }
+
+  renderSushi = () => {
+    // doesn't account for the full circle.. logic is flawed. 
+    if (this.state.index + 4 > this.props.sushi.length - 1) {
+      const remainingSushi = this.props.sushi.slice(this.state.index, this.props.sushi.length - 1)
+      const beginningSushi = this.props.sushi.slice(0, remainingSushi.length)
+      const currentSushi = remainingSushi.concat(beginningSushi)
+      return currentSushi.map(sushi => {
+        return <Sushi budget={this.props.budget} key={v4()} ateSushi={this.props.ateSushi} sushi={sushi}/>
+      })
+    } else {
+    const currentSushi = this.props.sushi.slice(this.state.index, this.state.index + 4)
+    return currentSushi.map(sushi => {
+      return <Sushi budget={this.props.budget} key={v4()} ateSushi={this.props.ateSushi} sushi={sushi}/>
+    })
+    }
+
+  }
+
+  handleClick = (e) => {
+    this.setState( prevState => {
+      return {
+        index: prevState.index + 4
+      }
+    })
+  }
+
+  render() {
+    return (
+      <Fragment>
+        <div className="belt">
+          {
+            this.renderSushi()
+          }
+          <MoreButton handleClick={this.handleClick}/>
+        </div>
+      </Fragment>
+    )
+  }
 }
-
-export default SushiContainer
